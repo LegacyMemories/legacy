783e7e4b2b31f03d611f8ec599f017240e2db79d
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2022-01-26 16:25:04 -0500

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-
 import './App.css';
-import Nav from "./components/Nav"
+//import Nav from "./components/Nav"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div className="App">
-  <Nav />
-    </div>
+    <Home />
   );
 }
 
