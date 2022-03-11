5bfd5af3a1c1b94d3676304ff35bc8339f1d132e
Val <vdecyatnik@gmail.com>
2022-03-10 16:38:33 -0800

    google oAuth

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,21 @@
 const passport = require("passport");
-
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+passport.serializeUser((user,done) =>{
+done(null,user.id)
+})
+
+
+passport.deserializeUser((id,done) =>{
+  User.findById(id)
+  .then(user=> {
+    done(null,user);
+  })
+
+})
 
 passport.use(
   new GoogleStrategy(
@@ -12,6 +25,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //already have user with googleprofileid
+          done(null, existingUser);
+        } else {
+          //create new user
+          new User({ googleId: profile.id }).save()
+          .then(user => done(null,user))
+        }
+      });
       console.log("accessToken", accessToken);
       console.log("refreshToken", refreshToken);
       console.log("profile", profile);
